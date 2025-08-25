91f64f347764d122a6b19d2106bf1c4e0264cff3
ANSHUMAN YADAV <145282790+Student-Anshuman@users.noreply.github.com>
2025-08-26 01:37:37 +0530

    event loop

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const http = require("http");
 
 const server = http.createServer((req, res) =>{
   console.log(req);
+  process.exit();  // stop event loop
+  
 });
 
-server.listen(3000);
+const PORT = 3000;
+server.listen(PORT, () => { 
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
 
