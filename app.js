95fffc7a6c69330971e47556f51602de9da36a0a
ANSHUMAN YADAV <145282790+Student-Anshuman@users.noreply.github.com>
2025-08-26 02:52:32 +0530

    in this section we learn how to write html,css in node js file and routing request

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 // Simple Nod.js server
 const http = require("http");
 
-const server = http.createServer((req, res) =>{
-  console.log(req.url, req.method, req.headers);
+const server = http.createServer((req, res) => {
+  // res.setHeader('Content-Type', 'json');
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Complete coding</title></head>');
   
+
+  if (req.url === '/') {
+    res.write('<body><h1>Welcome to home page</h1></body>');
+  } else if (req.url.toLowerCase() === '/products') {
+    res.write('<body><h1>Welcome to products page</h1></body>');
+    return res.end();
+  } else {
+    res.write('<body><h1>404 Page not found</h1></body>');
+    return res.end();
+  }
+  res.write('<body><h1>Hello Coder</h1></body>');
+res.write('</html>');
+  return res.end();
 
 });
 
