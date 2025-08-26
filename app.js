0207902e8c6294f4663dd06d874220e0ad0e8ae6
ANSHUMAN YADAV <145282790+Student-Anshuman@users.noreply.github.com>
2025-08-26 22:08:20 +0530

    learning routing request and taking user input

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 // Simple Nod.js server
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // res.setHeader('Content-Type', 'json');
 
-  res.setHeader('Content-Type', 'text/html');
+  
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Complete coding</title></head>');
+    
+    res.write('<body><h1>Enter your detail</h1>');
+    res.write('<form action="/submit-details" method = "POST" > ');
+    res.write('<input type="text" name="name" placeholder="Enter your name"> ');
+    res.write('<label for "gender"> Gender: </label> ');
+    res.write('<input type="radio" id="male" name="gender" value="male"> ');
+    res.write('<label for="male">Male</label> ');
+    res.write('<input type="radio" id="female" name="gender" value="female"> ');
+    res.write('<label for="female">Female</label> ');
+    res.write('<button type="submit">Submit</button> ');
+    res.write('</form> ');
+    res.write('</body > ');
+    res.write('</html>');
+    return res.end();
+   
+
+  } else if ( req.url.toLowerCase === '/submit-details' && req.method === 'POST') {
+    fs.writeFileSync("output.txt", "Anshuman")
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      // return res.end();
+    } 
+    
+   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>Complete coding</title></head>');
-  
-
-  if (req.url === '/') {
-    res.write('<body><h1>Welcome to home page</h1></body>');
-  } else if (req.url.toLowerCase() === '/products') {
-    res.write('<body><h1>Welcome to products page</h1></body>');
-    return res.end();
-  } else {
-    res.write('<body><h1>404 Page not found</h1></body>');
-    return res.end();
-  }
-  res.write('<body><h1>Hello Coder</h1></body>');
-res.write('</html>');
+  res.write('</html>');
   return res.end();
 
 });
